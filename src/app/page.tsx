9723a80302ad0cfa2791274387d799818325fe8b
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-13 23:56:19 +0530

    cross-origin-isolated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
         <h1>Quadratic - Prod (Auth0) - https://app.quadratichq.com</h1>
         <iframe
           src="https://app.quadratichq.com"
+          allow="cross-origin-isolated"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
           style={{ width: "100%", height: "100vh" }}
         />
       </div>
@@ -16,6 +18,8 @@ export default function Home() {
         </h1>
         <iframe
           src="https://app.quadratichq.com/file/3eb0a466-0445-4cff-94a7-006f10aeddc9"
+          allow="cross-origin-isolated"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
           style={{ width: "100%", height: "100vh" }}
         />
       </div>
@@ -24,6 +28,8 @@ export default function Home() {
         <h1>Quadratic - WorkOS - https://ayush-workos.quadratic-preview.com</h1>
         <iframe
           src="https://ayush-workos.quadratic-preview.com"
+          allow="cross-origin-isolated"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
           style={{ width: "100%", height: "100vh" }}
         />
       </div>
@@ -35,6 +41,8 @@ export default function Home() {
         </h1>
         <iframe
           src="https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589"
+          allow="cross-origin-isolated"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
           style={{ width: "100%", height: "100vh" }}
         />
       </div>
