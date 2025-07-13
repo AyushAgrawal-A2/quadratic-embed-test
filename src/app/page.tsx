760ffaf7451e037ac7da57fbeb61fc5cdbdf80f4
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-14 00:21:32 +0530

    width and height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,53 @@
 export default function Home() {
   return (
-    <div>
-      <div className="flex flex-col gap-4 items-center justify-center">
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col items-center justify-center">
         <h1>Quadratic - Prod (Auth0) - https://app.quadratichq.com</h1>
+
         <iframe
           src="https://app.quadratichq.com"
           allow="cross-origin-isolated"
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
-          style={{ width: "100%", height: "100vh" }}
+          style={{ width: "100%", height: "768px" }}
         />
       </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <h1>
           Quadratic - Prod (Public File) -
           https://app.quadratichq.com/file/3eb0a466-0445-4cff-94a7-006f10aeddc9
         </h1>
+
         <iframe
           src="https://app.quadratichq.com/file/3eb0a466-0445-4cff-94a7-006f10aeddc9"
           allow="cross-origin-isolated"
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
-          style={{ width: "100%", height: "100vh" }}
+          style={{ width: "100%", height: "768px" }}
         />
       </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <h1>Quadratic - WorkOS - https://ayush-workos.quadratic-preview.com</h1>
+
         <iframe
           src="https://ayush-workos.quadratic-preview.com"
           allow="cross-origin-isolated"
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
-          style={{ width: "100%", height: "100vh" }}
+          style={{ width: "100%", height: "768px" }}
         />
       </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <h1>
           Quadratic - WorkOS (Public File) -
           https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589
         </h1>
+
         <iframe
           src="https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589"
           allow="cross-origin-isolated"
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
-          style={{ width: "100%", height: "100vh" }}
+          style={{ width: "100%", height: "768px" }}
         />
       </div>
     </div>
