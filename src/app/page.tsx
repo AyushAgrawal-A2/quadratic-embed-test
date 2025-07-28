5cb8934a81264afe316168756eff33aed6111583
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-29 05:21:29 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,32 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-4">
       <div className="flex flex-col items-center justify-center">
+        <h1>Quadratic - WorkOS - https://ayush-workos.quadratic-preview.com</h1>
+
+        <iframe
+          src="https://ayush-workos.quadratic-preview.com"
+          allow="cross-origin-isolated"
+          // allow="cross-origin-isolated; storage-access"
+          // sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-storage-access-by-user-activation"
+          style={{ width: "80%", height: "768px" }}
+        />
+      </div>
+
+      {/* <div className="flex flex-col items-center justify-center">
+        <h1>
+          Quadratic - WorkOS (Public File) -
+          https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589
+        </h1>
+
+        <iframe
+          src="https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589"
+          allow="cross-origin-isolated; storage-access"
+          sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-storage-access-by-user-activation"
+          style={{ width: "80%", height: "768px" }}
+        />
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <h1>Quadratic - Prod (Auth0) - https://app.quadratichq.com</h1>
 
         <iframe
@@ -24,32 +50,7 @@ export default function Home() {
           sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-storage-access-by-user-activation"
           style={{ width: "80%", height: "768px" }}
         />
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <h1>Quadratic - WorkOS - https://ayush-workos.quadratic-preview.com</h1>
-
-        <iframe
-          src="https://ayush-workos.quadratic-preview.com"
-          allow="cross-origin-isolated; storage-access"
-          sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-storage-access-by-user-activation"
-          style={{ width: "80%", height: "768px" }}
-        />
-      </div>
-
-      <div className="flex flex-col items-center justify-center">
-        <h1>
-          Quadratic - WorkOS (Public File) -
-          https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589
-        </h1>
-
-        <iframe
-          src="https://ayush-workos.quadratic-preview.com/file/a9f803c6-fa0c-4823-8042-fafdbd9ec589"
-          allow="cross-origin-isolated; storage-access"
-          sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-storage-access-by-user-activation"
-          style={{ width: "80%", height: "768px" }}
-        />
-      </div>
+      </div> */}
     </div>
   );
 }
