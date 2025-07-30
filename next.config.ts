ce9b7956e322bbd605cfac82e8f02f4a9b766c19
AyushAgrawal-A2 <ayushagl06@gmail.com>
2025-07-30 16:40:03 +0530

    revert

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
+            value: "same-origin",
           },
         ],
       },
